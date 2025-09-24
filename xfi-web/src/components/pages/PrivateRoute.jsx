415ba3f93978eb4c8983705e38cd4fca6b6b6903
skillDeCoder <lastbackup877@gmail.com>
2025-09-24 20:46:53 +0100

    bot setup

--- xfi-web/src/components/pages/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { Route, Navigate, useNavigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  return !localStorage.authId ? <Navigate to="/" /> : <Outlet />;
-};
-
-export default PrivateRoute;
